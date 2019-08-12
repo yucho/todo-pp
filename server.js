85bd1ffeb57a773cb8b5039f430644b3b37478c9
yucho <yucho.ho@gmail.com>
2019-08-12 14:12:56 -0700

    Connect MongoDB Atlas

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
 const app = express();
 
 app.get('/', (req, res) => {
