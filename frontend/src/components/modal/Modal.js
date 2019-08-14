f022c07117ab192774eb018bbf215dcbc74e8110
yucho <yucho.ho@gmail.com>
2019-08-14 16:40:29 -0700

    Use createPortal for Modal

--- frontend/src/components/modal/Modal.js
+++ frontend/src/components/modal/Modal.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import * as styles from './Modal.module.css';
 
-const Modal = ({ open, close, ...props }) => {
+const Modal = (props) => {
+  const [modalElement] = useState(() => {
+    const root = document.createElement('div');
+    root.className = styles.root;
+    return root;
+  });
+
+  useEffect(() => {
+    document.body.appendChild(modalElement);
+    return () => document.body.removeChild(modalElement);
+  }, [modalElement]);
+
+  return createPortal(<ModalContainer {...props}/>, modalElement);
+};
+
+const ModalContainer = ({ open, close, children, ...props }) => {
   return <div
-    className={`${styles.container}${open ? ` ${styles.open}` : ''}`}
+    className={`${styles.background}${open ? ` ${styles.open}` : ''}`}
     onClick={() => close()}
-    {...props}
-  />
+  >
+    <div
+      className={`${styles.container}${open ? ` ${styles.open}` : ''}`}
+      {...props}
+    />
+  </div>
 };
 
 export default Modal;
