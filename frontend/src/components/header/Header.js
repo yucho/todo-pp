b32050fabad871b3ed5681269de376c14250dbd1
yucho <yucho.ho@gmail.com>
2019-08-14 21:47:10 -0700

    Style Header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import * as styles from './Header.module.css';
 
 const Header = () => {
-  return <header className={styles.header}>
+  const isAuthenticated = useSelector((state) => state.session.isAuthenticated);
+
+  return <header
+    className={`${styles.header}${isAuthenticated ? ` ${styles.loggedIn}` : ''}`}
+  >
     <h1>todo++</h1>
-    <p>
-      Your average to-do app, but ever so slightly better. <br />
-      <span className={styles.small}>It's free and always will be.</span>
-    </p>
+    {!isAuthenticated &&
+      <p>
+        Your average to-do app, but ever so slightly better. <br />
+        <span className={styles.small}>It's free and always will be.</span>
+      </p>
+    }
   </header>
 };
 
