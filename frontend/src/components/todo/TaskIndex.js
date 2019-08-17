ffcbd287b0c2bcfb7329d879b5da7c6a3789569b
yucho <yucho.ho@gmail.com>
2019-08-17 13:17:14 -0700

    Style date with moment.js add edit & delete icons

--- frontend/src/components/todo/TaskIndex.js
+++ frontend/src/components/todo/TaskIndex.js
@@ -1,25 +1,33 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import moment from 'moment';
 import * as styles from './TaskIndex.module.css';
+import iconEdit from './edit.svg';
+import iconDelete from './delete.svg';
 import { fetchTasks } from '../../actions/tasks-actions';
 
 const TaskIndex = () => {
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchTasks());
-  }, []);
-
   const tasks = useSelector((state) => state.tasks);
   const tasksArr = Object.keys(tasks)
     .map((_id) => {
       const { body, due } = tasks[_id];
+      const iconStyle = { width: '16px', height: '16px' };
       return <tr key={_id} className={styles.row}>
         <td><input type="checkbox" /></td>
         <td>{body}</td>
-        <td>{due}</td>
+        <td>{due && moment(due).format('Y/M/D')}</td>
+        <td>
+          <img style={iconStyle} alt="Edit task" src={iconEdit} />
+          <img style={iconStyle} alt="Delete task" src={iconDelete} />
+        </td>
       </tr>
     });
     // .sort()
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
   
   return <section>
     <table className={styles.table}>
@@ -28,6 +36,7 @@ const TaskIndex = () => {
           <th>Done</th>
           <th>Task</th>
           <th>Due</th>
+          <th />
         </tr>
         {tasksArr}
       </tbody>
